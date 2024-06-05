802e8a5ff6d2089892202dbe30c2da8e462bc5b1
Heyoka <github@heyoka.us>
2024-06-05 11:31:34 -0400

    Changed Google Doc reference from URL to ID

--- encrypt.sh
+++ encrypt.sh
@@ -3,18 +3,21 @@ rm -rf ./dist /tmp/w3w;
 mkdir ./dist /tmp/w3w /tmp/w3w/unzip;
 
 # Unpack Google Doc
-wget -O /tmp/w3w/google-doc.zip $GOOGLE_DOC;
+wget -O /tmp/w3w/google-doc.zip "https://docs.google.com/document/d/$GOOGLE_DOC_ID/export?format=zip";
 unzip /tmp/w3w/google-doc.zip -d /tmp/w3w/unzip/;
 
 # Substring Replacements
+PUBLISH_DATE=$(date +%Y-%m-%d);
 sed -i -e "s/SEED_PHRASE/$SEED_PHRASE/g" /tmp/w3w/unzip/CryptoAssets.html;
-sed -i -e "s/PUBLISH_DATE/$SEED_PHRASE/g" /tmp/w3w/unzip/CryptoAssets.html;
+sed -i -e "s/PUBLISH_DATE/$PUBLISH_DATE/g" /tmp/w3w/unzip/CryptoAssets.html;
+sed -i -e "s/METAMASK_WALLET/$METAMASK_WALLET/g" /tmp/w3w/unzip/CryptoAssets.html;
 
 # Encryption
 ./node_modules/staticrypt/cli/index.js -d ./dist/ -p "$ENCRYPTION_PASSWORD" /tmp/w3w/unzip/CryptoAssets.html;
 
 # Package dist
 rm /tmp/w3w/unzip/CryptoAssets.html;
+mv ./dist/CryptoAssets.html ./dist/index.html;
 cp -R /tmp/w3w/unzip/* ./dist/;
 
 # Upload
